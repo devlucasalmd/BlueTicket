4c831dcf3d9ec20618b8e094df5adbcf259e4009
devlucasalmd <lucas.amaral.almd@gmail.com>
2025-05-29 17:39:10 -0300

    first commit

---
+++ src/main/java/com/br/blueticket/BlueTicketApplication.java
@@ -0,0 +1,13 @@
+package com.br.blueticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/blueticket/BlueTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.blueticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
